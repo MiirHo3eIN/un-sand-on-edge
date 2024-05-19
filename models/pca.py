cc23865109c5a50aacdd7203ee6613ddb51e348e
MiirHo3eIN <amirhmoallemi@gmail.com>
2024-05-19 15:43:18 +0200

    pca in progress

---
+++ models/pca.py
@@ -0,0 +1,8 @@
+__doc__ = r""" 
+
+    @brief: This script contains all the classes and funcions related to the 
+    Principal Component Analysis implementations for train and prediction. 
+    
+     
+
+"""
